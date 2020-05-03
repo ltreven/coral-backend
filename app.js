991793a00cd40df49b532596cffa52f3dde3e5f4
Emiliano Lowe <emilianolowe5@gmail.com>
2020-05-03 14:59:41 +0200

    enabled CORS

--- app.js
+++ app.js
@@ -27,8 +27,14 @@ app.use('/v1/properties', propertiesRouter);
 app.use('/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
+});
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // error handler
